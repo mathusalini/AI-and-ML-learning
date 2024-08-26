e305df98763037d23d7c9b8b1c95ac2713315e2d
mathusalini <mathuzz2001@gmail.com>
2024-08-26 21:43:42 +0530

    commit

--- createa-folder.py
+++ createa-folder.py
@@ -1,16 +1,7 @@
-import subprocess
+import os
 
-# Your batch commands as a string
-batch_commands = """
-@echo off
-set /p folder_name="Enter the folder name: "
-set /p folder_path="Enter the path where the folder should be created: "
+# specify the path for the directory – make sure to surround it with quotation marks
+path = './projects'
 
-mkdir "%folder_path%\%folder_name%"
-
-echo Folder "%folder_name%" created at "%folder_path%"
-pause
-"""
-
-# Run the batch commands
-subprocess.run(batch_commands, shell=True, text=True)
+# create new single directory
+os.mkdir(path)
